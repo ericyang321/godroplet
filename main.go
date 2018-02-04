c1cda5470a66292037b1dc4d6eb37036b1386b82
Eric Yang <ericyang321@gmail.com>
2018-02-04 15:23:31 -0800

    Explored HTTP package, and played with some logging effects.

--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
 )
 
 func handler(res http.ResponseWriter, req *http.Request) {
-    res.WriteHeader(200)
-    res.Write([]byte("OK"))
+    // Echo HTTPS Request
+    fmt.Fprintf(res, "%s %s %s \n", req.Method, req.URL, req.Proto)
+
+    for k, v := range req.Header {
+        fmt.Fprintf(res, "Header[%q] = %q\n", k, v)
+        fmt.Fprintf(res, "Host = %q\n", req.Host)
+        fmt.Fprintf(res, "RemoteAddr = %q\n", req.RemoteAddr)
+    }
+
+    if err := req.ParseForm(); err != nil {
+        log.Print(err)
+    }
+
+    for k, v := range req.Form {
+        fmt.Fprintf(res, "Form[%q] = %q\n", k, v)
+    }
 }
 
 func main() {
     fmt.Println("Up and Running at :3000")
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":3000", nil)
+    log.Fatal(http.ListenAndServe(":3000", nil))
 }

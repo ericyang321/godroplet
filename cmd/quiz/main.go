79dcc8fff9310e2fc60dca536996fa3f5a13b509
Eric Yang <ericyang321@gmail.com>
2018-04-07 11:08:40 -0700

    opted to use ReadAll() instead of Read() in quiz direction, and rewrote code around that. ReadAll() could definitely destroy our RAM if some sneaky person loaded a super large exam CSV file, but for the sake of learning I'm just going to assume no one will use this program except me.

--- cmd/quiz/main.go
+++ cmd/quiz/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
-	"io"
 	"os"
-	"path"
-	// "time"
+	"strings"
 )
 
 // Error interface
@@ -14,49 +13,80 @@ type Error interface {
 	Error() string
 }
 
+type problem struct {
+	question string
+	answer   string
+}
+
 // func timer(seconds int) <-chan bool {
-//  done := make(chan bool)
-//  // Initiate timer
-//  go func() {
-//      time.Sleep(time.Duration(seconds) * time.Second)
-//      done <- true
-//  }()
-//  return done
+// 	done := make(chan bool)
+// 	// Initiate timer
+// 	go func() {
+// 		time.Sleep(time.Duration(seconds) * time.Second)
+// 		done <- true
+// 	}()
+// 	return done
 // }
 
-func getFile(s string) (*os.File, Error) {
-	dir := path.Join("/Users/ericyang/Documents/Home/go/src/github.com/ericyang321/godroplet/cmd/quiz", s)
-	return os.Open(dir)
+func terminate(s string) {
+	fmt.Println(s)
+	os.Exit(1)
+}
+
+func getFile() *os.File {
+	// dir := path.Join("/Users/ericyang/Documents/Home/go/src/github.com/ericyang321/godroplet/cmd/quiz", s)
+	// Any flag.String lists will throw
+	fileName := flag.String("csv", "problems.csv", "a CSV in the format of question, answer")
+	flag.Parse()
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		terminate(fmt.Sprintf("Failed to open CSV file: %s\n", *fileName))
+	}
+	return file
+}
+
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return problems
+}
+
+func checkAnswer(userAnswer string, realAnswer string) bool {
+	if userAnswer == realAnswer {
+		return true
+	}
+	return false
 }
 
 // Learn about: flags, CSV, OS, time package.
 func main() {
-	f, openErr := getFile("problems.csv")
-	if openErr != nil {
-		fmt.Println(openErr.Error())
-		return
+	csvFile := getFile()
+	defer csvFile.Close()
+	reader := csv.NewReader(csvFile)
+	lines, err := reader.ReadAll()
+	if err != nil {
+		terminate(fmt.Sprintf("Failed to parse theCSV file: %s \n", err.Error()))
 	}
-	defer f.Close()
-	reader := csv.NewReader(f)
-	var (
-		record   []string
-		readErr  Error
-		question string
-		answer   string
-		score    int
-	)
-	for {
-		record, readErr = reader.Read()
-		if readErr == io.EOF {
-			fmt.Println("End of Quiz! You scored", score)
-			return
-		} else if readErr != nil {
-			fmt.Println("Improper CSV Format", readErr)
-			return
-		}
-		question = record[0]
-		answer = record[1]
+	parsedProblems := parseLines(lines)
 
-		fmt.Println(question, answer)
+	score := 0
+	for i, entry := range parsedProblems {
+		var userAnswer string
+		fmt.Printf("Problem #%d: %s = ? \n", i+1, entry.question)
+		// Answer needs to be a pointer address because fmt.Scanf does
+		// direct assignment on user input. We're not in dynamic type land anymore...
+		fmt.Scanf("%s \n", &userAnswer)
+		isCorrect := checkAnswer(userAnswer, entry.answer)
+		if isCorrect == true {
+			score++
+		}
 	}
+
+	fmt.Printf("You scored %d out of %d \n", score, len(parsedProblems))
 }

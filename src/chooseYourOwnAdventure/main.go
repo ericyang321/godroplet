0f9c4dad6c3226e581840d7d298cd0f97a6dea12
Eric Yang <ericyang321@gmail.com>
2018-04-29 14:36:21 -0700

    Finish custom adventure gophercise 3. Learned:
    1. how to use html/template to generate HTML
    2. Unmarshal and utilize json with encoding/json
    3. * can be used both as a dereferencing operator and a pointer type
    indicator (Confusing design... possibly remnants of C++?)

--- src/chooseYourOwnAdventure/main.go
+++ src/chooseYourOwnAdventure/main.go
@@ -3,24 +3,38 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
-func buildTemplate(key string, blob map[string]interface{}) {
-
+type arc struct {
+	Title   string
+	Story   []string
+	Options []option
 }
 
-func createHandler(key string, blob map[string]interface{}) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+type option struct {
+	Text string
+	Arc  string
+}
 
+func createHandler(key string, blob *map[string]arc) http.HandlerFunc {
+	template := template.Must(template.ParseFiles("./template.html"))
+	chosenArc := (*blob)[key]
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		if err := template.Execute(w, chosenArc); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
-func slurpJSON() map[string]interface{} {
+func slurpJSON() map[string]arc {
 	raw, readErr := ioutil.ReadFile("./gopher.json")
-	blob := make(map[string]interface{})
+	blob := make(map[string]arc)
 	if readErr != nil {
 		fmt.Println(readErr.Error())
 		os.Exit(1)
@@ -36,8 +50,8 @@ func slurpJSON() map[string]interface{} {
 func main() {
 	blob := slurpJSON()
 	mux := http.NewServeMux()
-	for k := range blob {
-		mux.HandleFunc("/"+k, createHandler(k, blob))
+	for key := range blob {
+		mux.HandleFunc("/"+key, createHandler(key, &blob))
 	}
 	http.ListenAndServe("localhost:8080", mux)
 }

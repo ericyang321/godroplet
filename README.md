496f4914f4ee1e26ee1128f6339ab64c05597b51
Eric Yang <ericyang321@gmail.com>
2018-07-01 14:27:20 -0700

    - Updated hacker news API queries to fetch a list of top article IDs. - Updated README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fun with go
+
+A heroku deployed repo that lets me put multiple Golang projects up on [godroplet.io](https://godroplet.io/)
+
+Specific projects are under `/src` directory.
--- src/hn/main.go
+++ src/hn/main.go
@@ -1,11 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-func getPosts() {
-	res, err := http.Get("")
+	"github.com/kr/pretty"
+)
+
+const apiURL = "https://hacker-news.firebaseio.com/v0"
+
+func getTopStories(topIDs *[]int) error {
+	res, getErr := http.Get(apiURL + "/topstories.json")
+	if getErr != nil {
+		return getErr
+	}
+	defer res.Body.Close()
+	decodeErr := json.NewDecoder(res.Body).Decode(topIDs)
+	if decodeErr != nil {
+		return decodeErr
+	}
+	return nil
+}
+
+func filterThirty(topIDs []int) []int {
+	if len(topIDs) <= 30 {
+		return topIDs
+	}
+	onlyThirtyIds := make([]int, 30)
+	for i := 0; i < 30; i++ {
+		onlyThirtyIds[i] = topIDs[i]
+	}
+	return onlyThirtyIds
 }
 
 func main() {
-
+	topIDs := make([]int, 100)
+	fetchErr := getTopStories(&topIDs)
+	if fetchErr != nil {
+		panic(fetchErr.Error())
+	}
+	topThirtyIDs := filterThirty(topIDs)
+	pretty.Println(topThirtyIDs)
 }

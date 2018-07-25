350dae8108140938c2fba2335fea98d48da8bcfc
Eric Yang <ericyang321@gmail.com>
2018-07-24 21:21:42 -0700

    Remove abandoned trim project

--- src/sandbox/main.go
+++ src/sandbox/main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"strings"
-
-	"github.com/kr/pretty"
-	"golang.org/x/net/html"
-)
-
 func main() {
-	str := `<div className="woo"></div><div className="wah"></div>`
-	n, _ := html.ParseFragment(strings.NewReader(str), &html.Node{
-		Type: html.ElementNode,
-	})
 
-	pretty.Println(n)
-	// html.Render(os.Stdout, n[0])
 }
